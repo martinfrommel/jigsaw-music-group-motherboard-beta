4884f2f2e41daa277c6db2b95bc4fe2be1d4c3c7
martinfrommel <martin.frommel@gmail.com>
2023-12-03 00:28:49 +0100

    Add robots meta tags to AdminPage

--- web/src/pages/AdminPage/AdminPage.tsx
+++ web/src/pages/AdminPage/AdminPage.tsx
@@ -7,8 +7,8 @@ const AdminPage = () => {
       <MetaTags
         title="Admin"
         description="An admin section for managing the users"
+        robots={['noindex', 'nofollow']}
       />
-
       <UsersCell />
     </>
   )
